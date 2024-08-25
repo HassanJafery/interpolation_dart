44393fdd482a5b6acfebf33032ddd3cb8d5f3456
HassanJafery <mhassanrazajaffery@gmail.com>
2024-08-25 18:16:46 +0500

    initail commit - dart_data_types.dart

---
+++ dart_data_types.dart
@@ -0,0 +1,27 @@
+void main(){
+
+    int price = 86;
+    double add_price= 15;
+    bool isValue = true;
+    var item = 'titanium needle';
+    var item_names = ['car','suvs','drones','planes'];
+    var item_company = {'car':'100', 'suvs':'300'};
+    var heart_symbol = '\u2665';
+    var laugh_symbol = '\u{1f600}';
+    
+    print(price);
+    print('\n');
+    print(add_price);
+    print('\n');
+    print(isValue);
+    print('\n');
+    print(item);
+    print('\n');
+    print(item_names);
+    print('\n');
+    print(heart_symbol);
+    print('\n');
+    print(laugh_symbol);
+    print('\n');
+    
+}
